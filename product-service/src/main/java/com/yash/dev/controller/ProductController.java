d4c2d6db14518c9ccdd61b330460260d6622063d
Yashwanth Anand S <yashwanthanands@outlook.com>
2023-06-22 15:32:42 +0530

    Implemented the reduceQuantity endpoint logic

--- product-service/src/main/java/com/yash/dev/controller/ProductController.java
+++ product-service/src/main/java/com/yash/dev/controller/ProductController.java
@@ -10,8 +10,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -39,5 +41,13 @@ public class ProductController {
     public ResponseEntity<ProductResponse> getProductById(@PathVariable("id") long productId) {
         ProductResponse productResponse = productService.getProductById(productId);
         return new ResponseEntity<>(productResponse,HttpStatus.OK);
+    }
+
+    @PutMapping("/product/reduceQuantity/{id}")
+    public ResponseEntity<Void> reduceQuantity(
+            @PathVariable("id") long productId,
+            @RequestParam long quantity) {
+        productService.reduceQuantity(productId,quantity);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
--- product-service/src/main/java/com/yash/dev/service/ProductServiceImpl.java
+++ product-service/src/main/java/com/yash/dev/service/ProductServiceImpl.java
@@ -16,14 +16,15 @@ import org.springframework.stereotype.Service;
 
 @Service
 @Log4j2
-public class ProductServiceImpl implements ProductService{
+public class ProductServiceImpl implements ProductService {
 
     private ProductRepository productRepository;
 
     @Autowired
     public ProductServiceImpl(ProductRepository prdRepository) {
-        productRepository=prdRepository;
+        productRepository = prdRepository;
     }
+
     @Override
     public long addProduct(ProductRequest productRequest) {
         log.info("Adding Product ");
@@ -38,11 +39,32 @@ public class ProductServiceImpl implements ProductService{
 
     @Override
     public ProductResponse getProductById(long productId) {
-        log.info("Get the product for productId : {}",productId);
-        Product product=productRepository.findById(productId)
-                .orElseThrow(() -> new ProductServiceCustomException("Product with given id is not found","PRODUCT_NOT_FOUND"));
-        ProductResponse productResponse=new ProductResponse();
+        log.info("Get the product for productId : {}", productId);
+        Product product = productRepository.findById(productId)
+                .orElseThrow(() -> new ProductServiceCustomException("Product with given id is not found", "PRODUCT_NOT_FOUND"));
+        ProductResponse productResponse = new ProductResponse();
         copyProperties(product, productResponse);
         return productResponse;
     }
+
+    @Override
+    public void reduceQuantity(long productId, long quantity) {
+        log.info("Reduce Quantity {} for Id : {}", productId, quantity);
+        Product product
+                = productRepository.findById(productId)
+                .orElseThrow(() -> new ProductServiceCustomException(
+                        "Product with given id not found",
+                        "PRODUCT_NOT_FOUND"
+                ));
+        if (product.getQuantity() < quantity) {
+            throw new ProductServiceCustomException(
+                    "Product doesnot have sufficient quantity",
+                    "INSUFFICIENT_QUANTITY"
+            );
+        }
+        product.setQuantity(product.getQuantity() - quantity);
+        productRepository.save(product);
+        log.info("Product Quantity updated sucessfully");
+    }
 }
+
