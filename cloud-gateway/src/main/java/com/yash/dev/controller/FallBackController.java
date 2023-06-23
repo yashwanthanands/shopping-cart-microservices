43947adfcc16af194e14833c861fd6ab0468053b
Yashwanth Anand S <yashwanthanands@outlook.com>
2023-06-23 20:00:30 +0530

    Implemented circuit breaker in API Gateway

---
+++ cloud-gateway/src/main/java/com/yash/dev/controller/FallBackController.java
@@ -0,0 +1,27 @@
+package com.yash.dev.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author yashwanthanands
+ */
+
+@RestController
+public class FallBackController {
+
+    @GetMapping("/orderServiceFallBack")
+    public String orderServiceFallBack() {
+        return "Order Service is down";
+    }
+
+    @GetMapping("/productServiceFallBack")
+    public String productServiceFallBack() {
+        return "Product Service is down";
+    }
+
+    @GetMapping("/paymentServiceFallBack")
+    public String paymentServiceFallBack() {
+        return "Payment Service is down";
+    }
+}
